52840623725f6cc023f381d138c855f39ec4853a
theswinghobo <acapgalano@gmail.com>
2020-01-31 03:56:43 +0800

    Add user authentication

--- DCS Codex App Server/DCSCodex/urls.py
+++ DCS Codex App Server/DCSCodex/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
 from DCSCodexAdmin import views 
+from django.conf.urls import include
+from rest_framework.authtoken import views as view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', views.RegisterUser.as_view(), name='register')
+    path('register/', views.RegisterUser.as_view(), name='register'),
+    url(r'^login/', view.obtain_auth_token)
 ]
